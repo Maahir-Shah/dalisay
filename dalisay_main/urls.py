859c9e381c2a848c6b6592dfb7d9f47cc765f5c7
Maahir-Shah <72528611+Maahir-Shah@users.noreply.github.com>
2020-12-05 12:50:32 -0500

    Work on Contact Us and About Us pages

--- dalisay_main/urls.py
+++ dalisay_main/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('contact/', views.contact, name= 'contact'),
     path('checkout/', views.checkout, name= 'checkout'),
     path('process_order/', views.process_order, name= 'process_order'),
-    path('send_contact_form/', views.send_contact_form, name= 'contact_form'),
     path('process_payment/', views.process_payment, name= 'process_payment'),
     path('payment_success/', views.payment_success, name= 'payment_success'),
     path('payment_failure/', views.payment_failure, name= 'payment_failure'),
